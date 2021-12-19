f2174fa79fad0e072ff81c095f42b85fc912171f
Pedro Junqueira <pedrocj@gmail.com>
2021-12-20 06:52:25 +1030

    clean up actions file js

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 export default {
   async login(context, payload) {
-    // const body = JSON.stringify({
-    //   username: payload.username,
-    //   password: payload.password,
-    // });
     const config = {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -14,13 +10,6 @@ export default {
     const params = new URLSearchParams();
     params.append("username", payload.username);
     params.append("password", payload.password);
-
-    // console.log(body);
-    // const response = await fetch("http://127.0.0.1:8000/api/v1/users/token", {
-    //   method: "POST",
-    //   body: body,
-    // });
-    // console.log(response);
     const response = await axios({
       method: "post",
       url: "http://127.0.0.1:8000/api/v1/users/token",
